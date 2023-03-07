7522c93587f064ce3d87b36d57000542bb7930b9
vidyaballa5033 <vidyaballa503@gmail.com>
2023-03-07 11:29:50 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        System.out.println("addition " + sum);
+        System.out.println("subtraction  " + sub);
+        System.out.println("multiplication  " + mul);
+
+    }
+
+}
